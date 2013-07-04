07e23faf8dfc56083e3c6a9ad80f66ad8a4b8163
Andrew Lorente <andrew.lorente@gmail.com>
2013-07-04 09:53:39 -0700

    Json endpoint decorator for a cleaner controller

--- radlibs/web/controllers/demo_eval.py
+++ radlibs/web/controllers/demo_eval.py
@@ -2,50 +2,44 @@ from __future__ import unicode_literals
 
 import json
 from mock import patch
-from flask import request, make_response
+from flask import request
 from parsimonious.exceptions import IncompleteParseError
+from radlibs.parser import parse
 from radlibs.web import app
-from radlibs.expand import expand
+from radlibs.web.json_endpoint import json_endpoint
 
 
 @app.route('/demo_eval', methods=['POST'])
+@json_endpoint
 def demo_eval():
     try:
         rad = request.form['rad']
     except KeyError as e:
-        return make_response(json.dumps({
-            'status': 'error',
-            'error': "the 'rad' param is required",
-        }))
+        return _error("the 'rad' param is required")
     try:
         libs = request.form['libs']
         libs = json.loads(libs)
     except KeyError as e:
-        return make_response(json.dumps({
-            'status': 'error',
-            'error': "the 'libs' param is required",
-        }))
+        return _error("the 'libs' param is required")
     except ValueError as e:
-        return make_response(json.dumps({
-            'status': 'error',
-            'error': "'libs' param is not valid JSON: " + str(e),
-        }))
+        return _error("'libs' param is not valid JSON: " + str(e))
 
     with patch('radlibs.parser.load_lib', lambda name: libs[name]):
         try:
-            radlib = expand(rad)
+            radlib = str(parse(rad))
         except KeyError as e:
-            return make_response(json.dumps({
-                'status': 'error',
-                'error': "No such Library '{0}'".format(e.message),
-            }))
+            return _error("No such Library '{0}'".format(e.message))
         except IncompleteParseError as e:
-            return make_response(json.dumps({
-                'status': 'error',
-                'error': str(e),
-            }))
+            return _error(str(e))
 
-    return make_response(json.dumps({
+    return {
         'status': 'ok',
         'radlib': radlib,
-    }))
+    }
+
+
+def _error(error_string):
+    return {
+        'status': 'error',
+        'error': error_string,
+    }
---
+++ radlibs/web/json_endpoint.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+import json
+from functools import wraps
+from flask import make_response
+
+
+def json_endpoint(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        response = fn(*args, **kwargs)
+        if type(response) in [list, dict]:
+            response = json.dumps(response)
+
+        if type(response) in [unicode, str]:
+            response = make_response(response)
+        return response
+    return wrapper
