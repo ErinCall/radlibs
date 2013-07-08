2f1fd6fb3945552a3f5b2dd98ff62f131e6afbef
Andrew Lorente <andrew.lorente@gmail.com>
2013-07-08 06:31:05 -0700

    JSON endpoints shouldn't throw raw http400

--- radlibs/web/json_endpoint.py
+++ radlibs/web/json_endpoint.py
@@ -3,12 +3,16 @@ from __future__ import unicode_literals
 import json
 from functools import wraps
 from flask import make_response
+from werkzeug.exceptions import BadRequestKeyError
 
 
 def json_endpoint(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        response = fn(*args, **kwargs)
+        try:
+            response = fn(*args, **kwargs)
+        except BadRequestKeyError as e:
+            response = error_response("missing param '{0}'".format(e.message))
         if type(response) in [list, dict]:
             response = json.dumps(response)
 
